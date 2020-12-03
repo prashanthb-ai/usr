8aab3829b9d3c71912103570f5d8734593f5a610
Prashanth B <prashanth.b@wadhwaniai.org>
2020-12-03 20:45:48 +0530

    code: reshaping the face landmarks pandas frame via np

--- pytorch/annotator.py
+++ pytorch/annotator.py
@@ -1,10 +1,10 @@
 import pandas as pd
+import numpy as np
 
 landmarks_frame = pd.read_csv(
         '/home/beeps/rtmp/datasets/faces/face_landmarks.csv')
 
-# TODO: Print out the shape of the landmarks frame
-# Hint: Landmarks frame is stored as 68 data points of x and y, use np.reshape
+print (np.reshape(np.asarray(landmarks_frame.iloc[0, 1:]), (-1, 2)).shape)
 # TODO: Plot landmarks over jpgs
 
 
